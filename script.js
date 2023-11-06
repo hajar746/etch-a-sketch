91c87015a7e0a0620c5161ca14c79e4fb85a667b
hajar746 <132266440+hajar746@users.noreply.github.com>
2023-11-06 13:39:08 +0300

    add rainbow effect

--- script.js
+++ script.js
@@ -46,3 +46,21 @@ sketchPad.addEventListener("click", function (e) {
     });
   }
 });
+
+// RAINBOW button
+const btnRainbow = document.querySelector(".btn-rainbow");
+function randomColorFunc() {
+  return `rgb(${Math.floor(Math.random() * 255)}, ${Math.floor(
+    Math.random() * 255
+  )}, 
+    ${Math.floor(Math.random() * 255)})`;
+}
+
+btnRainbow.addEventListener("click", () => {
+  let divs = document.querySelectorAll(".divs");
+  for (let i = divs.length - 1; i >= 0; i--) {
+    divs[i].addEventListener("mouseenter", () => {
+      divs[i].style.backgroundColor = randomColorFunc();
+    });
+  }
+});
